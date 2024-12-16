54b2c465e15b7405cd68e99ce94cee3dfc9eeb0d
ChuchuSalazar <jessalaz@ucab.edu.ve>
2024-12-16 16:29:44 -0400

    Actualizaci├│n de c├│digo y preguntas

--- encuestas.py
+++ encuestas.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import random
 import datetime
-from firebase_admin import credentials, firestore, initialize_app, get_app
+from firebase_admin import credentials, firestore, initialize_app
 import os
 from dotenv import load_dotenv
 
@@ -10,16 +10,10 @@ from dotenv import load_dotenv
 load_dotenv()
 FIREBASE_CREDENTIALS = os.getenv("FIREBASE_CREDENTIALS")
 
-# Inicializar Firebase solo si no está inicializado
-try:
-    # Intenta obtener la app predeterminada
-    app = get_app()
-except ValueError:
-    # Si no existe, inicializa la app
-    cred = credentials.Certificate(FIREBASE_CREDENTIALS)
-    app = initialize_app(cred)
-
-db = firestore.client(app)
+# Inicializar Firebase
+cred = credentials.Certificate(FIREBASE_CREDENTIALS)
+initialize_app(cred)
+db = firestore.client()
 
 # Función para generar un ID aleatorio
 
@@ -35,12 +29,12 @@ df_preguntas = pd.read_excel('preguntas.xlsx')
 
 
 def guardar_respuestas(respuestas):
-    id_encuesta = generar_id()
+    # Asegurando un ID coherente con la estructura
+    id_encuesta = f"ID_{generar_id()}"
     fecha = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 
     # Crear un diccionario con todas las respuestas y los datos adicionales
     data = {
-        'ID': id_encuesta,
         'FECHA': fecha,
         'SEXO': respuestas.get('sexo', ''),
         'RANGO_EDA': respuestas.get('rango_edad', ''),
@@ -50,11 +44,12 @@ def guardar_respuestas(respuestas):
     }
 
     # Añadir las respuestas de las preguntas
-    for i, pregunta_id in enumerate(df_preguntas['item']):
-        data[f'AV{i+1}'] = respuestas.get(f'AV{pregunta_id}', '')
+    for i, row in df_preguntas.iterrows():
+        pregunta_id = row['item']
+        data[f'AV{pregunta_id}'] = respuestas.get(f'AV{pregunta_id}', '')
 
     # Guardar en Firebase
-    db.collection('respuestas').document(str(id_encuesta)).set(data)
+    db.collection('respuestas').document(id_encuesta).set(data)
 
 # Función para mostrar la encuesta
 
@@ -69,16 +64,16 @@ def mostrar_encuesta():
     # Mostrar los datos demográficos en forma horizontal
     st.header("Datos Demográficos")
 
-    sexo = st.radio("Sexo:", ['M - Masculino',
-                    'F - Femenino', 'O - Otro'], key='sexo')
+    sexo = st.radio("Sexo:", ['M - Masculino', 'F - Femenino',
+                    'O - Otro'], key='sexo', horizontal=True)
     respuestas['sexo'] = sexo.split()[0]
 
     rango_edad = st.radio("Rango de edad:", [
-                          '1 - 18-25', '2 - 26-35', '3 - 36-45', '4 - 46-60', '5 - Más de 60'], key='rango_edad')
+                          '1 - 18-25', '2 - 26-35', '3 - 36-45', '4 - 46-60', '5 - Más de 60'], key='rango_edad', horizontal=True)
     respuestas['rango_edad'] = rango_edad.split()[0]
 
     rango_ingreso = st.radio("Rango de ingresos (US$):", [
-                             '1 - 0-300', '2 - 301-700', '3 - 701-1100', '4 - 1101-1500', '5 - 1501-3000', '6 - Más de 3000'], key='rango_ingreso')
+                             '1 - 0-300', '2 - 301-700', '3 - 701-1100', '4 - 1101-1500', '5 - 1501-3000', '6 - Más de 3000'], key='rango_ingreso', horizontal=True)
     respuestas['rango_ingreso'] = rango_ingreso.split()[0]
 
     ciudad = st.selectbox("Ciudad:", ['1 - Ciudad A', '2 - Ciudad B',
@@ -86,7 +81,7 @@ def mostrar_encuesta():
     respuestas['ciudad'] = ciudad.split()[0]
 
     nivel_educativo = st.radio("Nivel educativo:", [
-                               '1 - Primaria', '2 - Secundaria', '3 - Universitario', '4 - Postgrado'], key='nivel_educativo')
+                               '1 - Primaria', '2 - Secundaria', '3 - Universitario', '4 - Postgrado'], key='nivel_educativo', horizontal=True)
     respuestas['nivel_educativo'] = nivel_educativo.split()[0]
 
     # Mostrar las preguntas numeradas y enmarcadas
@@ -95,7 +90,7 @@ def mostrar_encuesta():
     for i, row in df_preguntas.iterrows():
         pregunta_id = row['item']
         pregunta_texto = row['pregunta']
-        escala = ['No seleccionar'] + row['posibles respuestas'].split(',')
+        escala = row['posibles respuestas'].split(',')
 
         st.markdown(f"**Pregunta {i+1}:**")
         st.markdown(f'<div style="border: 2px solid #add8e6; padding: 10px; border-radius: 5px; font-size: 16px; font-family: Arial, sans-serif;">{
@@ -107,8 +102,8 @@ def mostrar_encuesta():
     # Botón para enviar las respuestas
     if st.button("Enviar"):
         # Validar que todas las preguntas hayan sido respondidas
-        preguntas_faltantes = [f"Pregunta {i+1}" for i, row in df_preguntas.iterrows(
-        ) if respuestas.get(f'AV{row["item"]}', '') == 'No seleccionar']
+        preguntas_faltantes = [f"Pregunta {
+            i+1}" for i, row in df_preguntas.iterrows() if not respuestas.get(f'AV{row["item"]}', None)]
 
         if preguntas_faltantes:
             st.error(f"Por favor, responde las siguientes preguntas: {
